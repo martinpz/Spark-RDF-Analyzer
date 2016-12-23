8eba8ba150038e6f3e6992311100c4aedd9ab46a
Marco Probst <marco-probst-12@web.de>
2016-12-23 10:36:27 +0100

    Removes unused import.

--- Spark RDF Analyzer - Standalone Version/src/main/java/rdfanalyzer/spark/Configuration.java
+++ Spark RDF Analyzer - Standalone Version/src/main/java/rdfanalyzer/spark/Configuration.java
@@ -18,7 +18,6 @@ package rdfanalyzer.spark;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import org.apache.spark.SparkConf;
