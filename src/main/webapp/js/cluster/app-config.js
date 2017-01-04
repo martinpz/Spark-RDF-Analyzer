a27df5dfb2200bd677350c022f7fb054c4832884
Marco Probst <marco-probst-12@web.de>
2017-01-04 13:32:39 +0100

    Uses different app-config files for cluster and localhost.

---
+++ src/main/webapp/js/cluster/app-config.js
@@ -0,0 +1,9 @@
+var CONFIG = {
+	prot : 'http',
+	site : 'isydney.informatik.uni-freiburg.de',
+	port : '8080',
+	rest : 'spark-rdfanalyzer2/rest/ALL'
+};
+
+var REST_API = CONFIG.prot + "://" + CONFIG.site + ":" + CONFIG.port + "/"
+		+ CONFIG.rest + "/";
