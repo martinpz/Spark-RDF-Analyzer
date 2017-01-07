86aa9acfa4f3514eca1a9288517fc212c4db7257
Marco Probst <marco-probst-12@web.de>
2017-01-07 12:54:29 +0100

    Implements stubs for neighborhood computation.

---
+++ src/main/java/rdfanalyzer/spark/Neighborhood.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 University of Freiburg.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package rdfanalyzer.spark;
+
+import org.json.JSONObject;
+
+/**
+ * This class uses a detailed and advanced method to calculate number of nodes.
+ */
+public class Neighborhood {
+	/**
+	 * Computes the neighbors w.r.t. their distance and amount.
+	 * 
+	 * @param graph
+	 *            The name of the graph to use.
+	 * @param centralNode
+	 *            The URI of the central node.
+	 * @param num
+	 *            How many neighbors to return.
+	 * @param distance
+	 *            How far away a neighbor may be.
+	 * 
+	 * @return A JSONObject of the URIs of the neighbors.
+	 */
+	public static JSONObject getNeighbors(String graph, String centralNode, int num, int distance) {
+		if (num <= 0) {
+			throw new IllegalArgumentException("Requested number of neighbors must be greater than zero.");
+		}
+
+		if (distance <= 0) {
+			throw new IllegalArgumentException("Distance must be greater than zero.");
+		}
+
+		JSONObject jsonObj = new JSONObject();
+
+		jsonObj.put("neighborMaybe", "This/is/a/simple/test");
+		jsonObj.put("neighborMaybe", "Here/is/another/URI/maybe");
+
+		return jsonObj;
+	}
+}
--- src/main/webapp/js/browser.js
+++ src/main/webapp/js/browser.js
@@ -1,3 +1,6 @@
+var loader = '<div class="progress progress-striped active page-progress-bar"><div class="progress-bar" style="width: 100%;"></div></div>';
+
+// ########################## Entry Point ##########################
 function showAutocompletionModal() {
 	var input = $('#entryNode').val();
 
@@ -9,9 +12,8 @@ function showAutocompletionModal() {
 		$('#btnStartBrowsing').hide();
 	} else {
 		$('#modalTitle').text('Select your entry point!');
-		$('#modalBody')
-				.html(
-						'<p>Currently computing the autocomplete suggestions ...</p><div class="progress progress-striped active page-progress-bar"><div class="progress-bar" style="width: 100%;"></div></div>');
+		$('#modalBody').html(
+				'<p>Computing the autocomplete suggestions ...</p>' + loader);
 		$('#btnStartBrowsing').show();
 
 		var xhttp = new XMLHttpRequest();
@@ -22,8 +24,8 @@ function showAutocompletionModal() {
 			}
 		}
 
-		xhttp.open("GET", REST_API + "autoComplete/" + getCookie('graphName')
-				+ "/" + input + "/Node", true);
+		xhttp.open('GET', REST_API + 'autoComplete/' + getCookie('graphName')
+				+ '/' + input + '/Node', true);
 		xhttp.send();
 	}
 }
@@ -37,26 +39,57 @@ function startBrowsing(event) {
 		return false;
 	}
 
-	var selectedText_arr = selectedText.split(":"); // u52,<http,//www.ins.cwi.nl/sib/user/u52>
+	var selectedText_arr = selectedText.split(':'); // u52,<http,//www.ins.cwi.nl/sib/user/u52>
 	var selectedValue = selectedText_arr[0]; // u52
-	var selectedURI = selectedText_arr[1] + ":" + selectedText_arr[2]; // <http://www.ins.cwi.nl/sib/user/u52>
+	var selectedURI = selectedText_arr[1] + ':' + selectedText_arr[2]; // <http://www.ins.cwi.nl/sib/user/u52>
 
 	// Close modal.
 	$('#btnCloseModal').click();
 
-	// Prepare Browser.
-	$('#browserModalBody').text('Selected URI: ' + selectedURI);
+	prepareBrowser(selectedValue, selectedURI);
 }
 
+// ########################## RDF Browser ##########################
+function prepareBrowser(selectedValue, centralNode) {
+	var numNeighbors = 5;
+	var xhttp = new XMLHttpRequest();
+
+	$('#browserModalBody').html(
+			'<p>Computing the neighbors for ' + selectedValue + ' ...</p>'
+					+ loader);
+
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			displayNodes(centralNode, xhttp.responseText);
+		}
+	}
+
+	xhttp.open('GET', REST_API + 'directNeighbors/' + getCookie('graphName')
+			+ '?centralNode=' + encodeURIComponent(centralNode)
+			+ '&numNeighbors=' + numNeighbors + '&distance=1', true);
+	xhttp.send();
+}
+
+function displayNodes(centralNode, neighbors) {
+	console.log('Received message ...');
+	console.log('CentralNode=' + centralNode);
+	console.log('--- neighbors ---');
+	console.log(neighbors);
+
+	$('#browserModalBody').text('Central Node: ' + centralNode);
+	$('#browserModalBody').after('<div>' + neighbors + '</div>');
+}
+
+// ########################## Utility Functions ##########################
 function getGraphName() {
-	$("#GraphName").html(getCookie('graphName'));
+	$('#GraphName').html(getCookie('graphName'));
 }
 
 function getCookie(name) {
-	var value = "; " + document.cookie;
-	var parts = value.split("; " + name + "=");
+	var value = '; ' + document.cookie;
+	var parts = value.split('; ' + name + '=');
 
 	if (parts.length == 2) {
-		return parts.pop().split(";").shift();
+		return parts.pop().split(';').shift();
 	}
 }
