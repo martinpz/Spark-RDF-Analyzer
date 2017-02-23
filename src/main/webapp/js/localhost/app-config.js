5056484f52bb03aa777b2cad8592e9494c37aa12
Marco Probst <marco-probst-12@web.de>
2017-02-23 10:36:48 +0100

    Sets the texts for graph loading dynamically.

--- src/main/webapp/js/localhost/app-config.js
+++ src/main/webapp/js/localhost/app-config.js
@@ -1,9 +1,15 @@
-var CONFIG = {
+const CONFIG = {
 	prot : 'http',
 	site : '127.0.0.1',
 	port : '8080',
 	rest : 'spark-rdfanalyzer2/rest/ALL'
 };
 
-var REST_API = CONFIG.prot + "://" + CONFIG.site + ":" + CONFIG.port + "/"
+const REST_API = CONFIG.prot + "://" + CONFIG.site + ":" + CONFIG.port + "/"
 		+ CONFIG.rest + "/";
+
+const NEW_GRAPH = {
+	label: 'The local path to the folder that contains the graph data',
+	examplePath: 'e.g. /home/data/dbpedia',
+	exampleName: 'e.g. DBpedia'
+}
